08a0eb90dced526b4a6f371bc26bd4f4c677a0cb
shahzad67432 <shaa1891640@gmail.com>
2024-03-15 20:12:37 +0500

    type errors

--- blog-id/src/components/Button.tsx
+++ blog-id/src/components/Button.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Button: React.FC = () => {
-  return (
-    <Link to={'/publish'} style={{
-      fontSize: '1rem',
-      backgroundColor: '#3490DC',
-      display: 'inline-block',
-      justifyContent: 'center',
-      alignItems: 'center',
-      border: 'none',
-      outline: 'none',
-      textDecoration: 'none',
-      marginRight: '1rem',
-      color: '#FFF',
-      padding: '0.5rem 1rem',
-      borderRadius: '4px',
-      fontFamily: 'sans-serif',
-      textAlign: 'center',
-    }}>
-      Publish
-    </Link>
-  );
-};
+export const Button = () => {
+    const buttonStyle: React.CSSProperties = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "none",
+        outline: "none",
+        textDecoration: "none",
+        marginRight: "5px",
+        backgroundColor: "#15803c",
+        color: "#ffffff",
+        padding: "0.5rem 1rem",
+        fontSize: "1.5rem",
+        borderRadius: "9999px",
+        fontWeight: "500",
+        textAlign: "center",
+        transition: "background-color 0.3s",
+        paddingRight: "10px", // Corrected property name
+        fontFamily: "sans-serif"
+    };
 
-export default Button;
+    const buttonHoverStyle: React.CSSProperties = {
+        backgroundColor: "#15803c",
+    };
+
+    return (
+        <div style={{ paddingRight: "20px" }}>
+            <Link to={'/publish'} style={{ ...buttonStyle, ...buttonHoverStyle, fontSize: "15px" }}>
+                Publish
+            </Link>
+        </div>
+    );
+};
